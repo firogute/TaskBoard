a474cedcc0fbde3f10b2141824d5d90e6ccb6e0a
Firomsa Guteta <firomsaguteta12@gmail.com>
2025-03-03 21:44:21 +0300

    Add the add new task modal

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const openBtn = document.querySelector(".open-modal-btn");
 const modal = document.querySelector(".modal-overlay");
+const modalWrapper = document.querySelector(".modal-wrapper");
 const closeBtn = document.querySelector(".close-modal-btn");
 
 function openModal() {
@@ -8,9 +9,20 @@ function openModal() {
 
 function closeModal(e, clickedOutside) {
   if (clickedOutside) {
-    if (e.target.classList.contains("modal-overlay"))
+    if (e.target.classList.contains("modal-overlay")) {
+      modalWrapper.classList.add("closing");
+      setTimeout(() => {
+        modal.classList.add("hide");
+        modalWrapper.classList.remove("closing");
+      }, 300);
+    }
+  } else {
+    modalWrapper.classList.add("closing");
+    setTimeout(() => {
       modal.classList.add("hide");
-  } else modal.classList.add("hide");
+      modalWrapper.classList.remove("closing");
+    }, 300);
+  }
 }
 
 openBtn.addEventListener("click", openModal);
