654310db52d9d880a3b420d7a2b3b66aa3ac7101
Firomsa Guteta <firomsaguteta12@gmail.com>
2025-03-07 22:08:57 +0300

    Fixed server path for deployment

--- servers/app.js
+++ servers/app.js
@@ -8,6 +8,7 @@ import { createClient } from "@supabase/supabase-js";
 import dotenv from "dotenv";
 dotenv.config();
 
+console.log(process.env.SUPABASE_URL);
 const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_KEY = process.env.SUPABASE_KEY;
 
